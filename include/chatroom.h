34f69317d2d07130cf55e5e43d8c08f1db148846
lean001 <leannong@gmail.com>
2015-06-15 16:15:06 +0800

    add shm's function and reset sem's function

--- include/chatroom.h
+++ include/chatroom.h
@@ -1,6 +1,19 @@
 #ifndef __chatroom_h
 #define __chatroom_h
 
-/*读写函数*/
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <arpa/inet.h>
+#include <signal.h>
+#include <errno.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/shm.h>
+#include <fcntl.h>
+
+void error_delt(const char *err_str , int line);
 
 
+#endif
